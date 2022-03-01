07b4273bc3ac7b1178e80387c852ae174a17ef3f
Abram-green <54475167+Abram-green@users.noreply.github.com>
2022-03-01 18:59:06 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,74 +1,56 @@
-
+#global
+token = "token"
+prefix = "prefix"
+guild_id = guildid
+#send bot
+trade_timeout = 120
+content_timeout = 300
+photo_timeout = 5
+advert_timeout = 600
+anons_timeout = 600
+role_id = roleid
+disable_role = roleid
+advert_channel_id = roleid
+trade_channel_id = channelid
+photo_channel_id = channelid
+content_channel_id = channelid
+anons_channel_id = channelid
+audit_channel_id = channelid
+sinking_channel_id = channelid
+courts_channel_id = channelid
+choice_role_message_id = messageid
+create_voice_channel_id = messageid
+create_voice_category_id = messageid
+pinging_role_content = roleid
+pinging_role_advert = roleid
+pinging_role_trade = roleid
+main_photo_url = "imageurl"
+trade_photo_url = "imageurl"
+content_photo_url = "imageurl"
+photo_photo_url = "imageurl"
+advert_photo_url = "imageurl"
+filename = 'word list.txt'
+default_role_id = roleid
+online_role_id = roleid
+sponsor_role_id = roleid
+nitro_role_id = roleid
+mute_immune_roles = [roleid]
+message_roles = {
+    roleid: "rolename",
+}
+#sql checker bot
+sql_data = {
+    "host": "hostip",
+    "port": 'port',
+    "user": "username",
+    "password": "userpassword",
+    "database": "dbname"
+}
 permissions = {
-    884449396501139548: "group1",
-    884449419875995668: "group2",
-    884449439463387187: "group3"
+roleid: "permissinonname",
+
 }
 server = "global"
 world = "global"
-token = "NjcyODgzODgzMzUxNjA1Mjg4.XjR98w.hGO3a8L8xBjXSVt6cftDTdZ0WtU"
-TOKEN = token
-#token = "ODI1NDI4OTY1MjgzMjAxMDg0.YF9yoQ.FudcDBhSWFJGsjyiq86P5BD61eA"
-prefix = "prefix"
-trade_timeout = 60
-content_timeout = 60
-photo_timeout = 60
-advert_timeout = 60
-anons_timeout = 60
-guild_id = 631100451999318046
-role_id = 824635506091622473
-disable_role = 801100803753771070
-advert_channel_id = 906172037599465502
-trade_channel_id = 90617203759942
-photo_channel_id = 90617203759942
-content_channel_id = 9061720375992
-anons_channel_id = 9061720375992
-audit_channel_id = 908704777230233630
-sinking_channel_id = 908704777230233630
-choice_role_message_id = 941770927945953309
-create_voice_channel_id = 940664188198604801
-create_voice_category_id = 940664155327823892
-courts_channel_id = 908704777230233630
-main_photo_url = "https://cs5.pikabu.ru/images/big_size_comm/2015-11_4/1447696666173234614.png"
-trade_photo_url = "http://www.rosphoto.com/images/u/articles/1510/4_8.jpg"
-content_photo_url = "http://www.rosphoto.com/images/u/articles/1510/4_8.jpg"
-photo_photo_url = "http://www.rosphoto.com/images/u/articles/1510/4_8.jpg"
-advert_photo_url = "http://www.rosphoto.com/images/u/articles/1510/4_8.jpg"
-filename = 'word list.txt'
-pinging_role_trade = 941658248929480744
-pinging_role_content = 941658248929480744
-pinging_role_advert = 941658248929480744
-sponsor_role_id = 760187302130876522
-nitro_role_id = 749651559180140544
-sql_data = {
-    "host": "51.38.114.122",
-    "port": '3306',
-    "user": "faithcraft",
-    "password": "40b9tume04but8904ub0e8utb09e4",
-    "database": "faithcraft"
-}
-default_role_id = 824635506091622473
-online_role_id = 884449396501139548
-mute_immune_roles = [672034708233650176]
 iphost_faith = "mc.faithcraft.ru"
 iphost_faithtest = "test.faithcraft.ru"
-message_roles = {
-    863151707344601098: "Администрация",
-    932809516117008414: "Призедент",
-    835467497284698183: "Глава ФБР",
-    931969645844504637: "Судья",
-    925443959872978944: "Правительство",
-    826769900781436958: "Модерация",
-    831190606138572840: "ФБР"
-}
-permissions = {
-863151707344601098: "admin",
-826769900781436958: "mooderator",
-831190606138572840: "police",
-760187302130876522: "sponsor",
-932809516117008414: "president",
-936351710367076424: "kgb",
-936349292912865311: "kgb",
-835467497284698183: "police",
-
-}
